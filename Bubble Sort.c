f8517baa668f6beeb66a0c3ab4423a8da8686720
darsigangothri06 <darsigangothri@gmail.com>
2021-11-13 20:00:07 +0530

    Add: Bubble Sort

---
+++ Bubble Sort.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+int main()
+{
+    int a[20],i,j,temp,n;
+    printf("Enter the size of the arrays: ");
+    scanf("%d", &n);
+    for(i = 0; i < n; i++)
+    {
+        printf("Enter %d element: ",i+1);
+        scanf("%d",&a[i]);
+    }
+    for(i = 1; i < n; i++)
+    {
+        for(j = 0; j < n - i; j++)
+        {
+            if (a[j] < a[j+1])
+            {
+                temp = a[j];
+                a[j] = a[j+1];
+                a[j+1] = temp;
+            }
+        }
+    }
+    printf("\nSorted array is: \n");
+    for(i=0;i<n;i++) printf("%5d",a[i]);
+
+    return 0;
+}
