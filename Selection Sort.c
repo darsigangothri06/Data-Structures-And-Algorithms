235059dfb40acc323603ba21b883b2cc1958a0e8
darsigangothri06 <darsigangothri@gmail.com>
2021-11-13 22:10:51 +0530

    Add: Selection Sort

---
+++ Selection Sort.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int main()
+{
+    int a[20],n,i,j,k,sl,temp;
+    printf("\n Enter size of array: ");
+    scanf("%d",&n);
+    for(i = 0; i < n; i++)
+    {
+        printf("\nEnter %d element: ",i+1);
+        scanf("%d",&a[i]);
+    }
+    for(i = 0; i < n; i++)
+    {
+        sl = i;
+        for(k = i+1; k < n; k++)
+        {
+            if (a[k] > a[i]) j = k;
+        }
+        temp = a[sl];
+        a[sl] = a[j];
+        a[j] = temp;
+    }
+    printf("\n Sorted array using Selection sort is: ");
+    for(i = 0; i < n; i++) printf("%5d",a[i]);
+
+    return 0;
+}
