94f6b617b4d2f0b74fb31e601b42492874cfaa96
Darsi Gangothri <77739656+darsigangothri06@users.noreply.github.com>
2021-11-17 06:57:00 +0530

    Update Bubble Sort.c

--- Bubble Sort.c
+++ Bubble Sort.c
@@ -10,7 +10,7 @@ int main()
         printf("Enter %d element: ",i+1);
         scanf("%d",&a[i]);
     }
-    for(i = 1; i < n; i++)
+    for(i = 1; i < n - 1; i++)
     {
         for(j = 0; j < n - i; j++)
         {
