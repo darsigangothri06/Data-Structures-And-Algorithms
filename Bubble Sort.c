89077e9ecb7c4bf84716c6eacb4dbb9d3dac19ed
Darsi Gangothri <77739656+darsigangothri06@users.noreply.github.com>
2021-11-17 06:55:47 +0530

    Update Bubble Sort.c

--- Bubble Sort.c
+++ Bubble Sort.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-
+# Bubble sort - Ascending Order
 int main()
 {
     int a[20],i,j,temp,n;
@@ -14,7 +14,7 @@ int main()
     {
         for(j = 0; j < n - i; j++)
         {
-            if (a[j] < a[j+1])
+            if (a[j] > a[j+1])
             {
                 temp = a[j];
                 a[j] = a[j+1];
