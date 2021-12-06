f5690a083802e373a97335eab1842bd3b1015b14
darsigangothri06 <darsigangothri@gmail.com>
2021-12-06 16:53:34 +0530

    Add: Infix to Postfix Conversion

--- InfixToPostfix.py
+++ InfixToPostfix.py
@@ -1,34 +1,46 @@
 # Converting an Infix Expression to Postfix expression.
 Infix = input("Enter an Infix expression: ")
-Operators1 = ['/','%','*']
-Operators2 = ['+','-']
+Ranks = {'/': 1, '%':1, '*':1, '+': 0, '-': 0}
 Postfix = []
 Stack = []
 
 for i in Infix:
+    # if character is an alphabet --- then add it into postfix directly
     if i.isalpha():
         Postfix.append(i)
-        #print("Is alpha: ", Stack, Postfix)
+    # if character is an open parantheses or stack is empty, then add i into stack
     elif len(Stack) == 0 or i == '(' or Stack[-1] == '(':
         Stack.append(i)
-        #print("Is ( or Stack is ) or Length is 0: ", Stack, Postfix)
+    # if i is ')' then until we get an '(' pop the stack and push into postfix expression
     elif i == ')':
         while Stack[-1] != '(':
             Postfix.append(Stack.pop())
         Stack.pop()
-        #print("Is i is ): ", Stack, Postfix)
+    # if i is an operator, then we have to check the priority of the operators
     else:
-        Rank = 2 if i in Operators1 else 1
-        RankStack = 2 if Stack[-1] in Operators1 else 1
+        Rank = Ranks[i]
+        RankStack = Ranks[Stack[-1]]
+        # Rules --- A lower priority operator can accept a higher priority operator
         if RankStack < Rank:
             Stack.append(i)
-            #print("Is Rank > RankStack: ", Stack, Postfix, Rank, RankStack)
+        
         else:
-            Postfix.append(Stack.pop())
-            Stack.append(i)
-            #print("Is Rank < RankStack: ", Stack, Postfix, Rank, RankStack)
+            # If, Rank of Stack[-1] is greater than Rank of i, then Pop the stack and push it into the Postfix expression
+            if RankStack > Rank:
+                Postfix.append(Stack.pop())
+                RankStack = Ranks[Stack[-1]]
+                # After deleting the higher priority operator, we have to check with it's next Rank, if it is equal, then again Pop it and push into Postfix
+                if RankStack == Rank:
+                    Postfix.append(Stack.pop())
+                    Stack.append(i)
+                else:
+                    Stack.append(i)
+            else:
+                # If Rank of Stack[-1] is equal to Rank of incoming operator, then Pop the stack and push the result into postfix and add i into Stack
+                Postfix.append(Stack.pop())
+                Stack.append(i)
+# Adding the remaining operators from stack into Postfix expression
 while len(Stack) != 0:
     Postfix.append(Stack.pop())
-#print("Is Final:  ", Stack, Postfix)
 print()
-print("Given", Infix, " Postfix expression is: ", ''.join(Postfix))
+print("Given Infix Expression is: ", Infix, "\nPostfix expression is: ", ''.join(Postfix))
