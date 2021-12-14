8f8796350d6b8abc6fff899fbbf984a43dfa9405
darsigangothri06 <darsigangothri@gmail.com>
2021-12-14 18:33:23 +0530

    Add: SLL ADD NODE AT POSITION

--- SLL_Add_at_position.c
+++ SLL_Add_at_position.c
@@ -11,28 +11,42 @@ typedef struct TAG NODE;
 
 NODE* Add_AT_POS(NODE *START, int X, int POS)
 {
+    NODE *TEMP, *p;
     int i;
-    NODE *p, *TEMP;
-    TEMP = (NODE *)malloc(sizeof(NODE));
+    TEMP = (NODE*)malloc(sizeof(NODE));
     TEMP -> DATA = X;
     TEMP -> LINK = NULL;
-
-    if(START == NULL) START = TEMP;
-    else
+    if(START == NULL) START = TEMP;  // If start is empty
+    else if(POS == 0)  // Add node at begin
+    {
+        TEMP -> LINK = START;
+        START = TEMP;
+    }
+    else  // Add node at certain position
     {
         p = START;
-        for(i = 1; i <= POS; i++) p = p -> LINK;
+        for(i = 1; i < POS; i++)  // moving p to position.
+        {
+            p = p -> LINK;
+            if(p == NULL) // if position is greater than no:of nodes, then when moving pointer p, we get NULL. We get last node.
+            {
+                printf("Invalid Position, Position is greater than Number of nodes of Linked List to insert.\n");
+                return START;
+            }
+        }
         TEMP -> LINK = p -> LINK;
         p -> LINK = TEMP;
     }
 
+    // DISPLAYING THE LINKED LIST
     p = START;
-    while (!(p -> LINK == NULL))
+    printf("Begin ->");
+    while (p != NULL)
     {
-        printf("%d\t", p -> DATA);
+        printf(" %d -> ", p -> DATA);
         p = p -> LINK;
     }
-    printf("%d\n", p -> DATA);
+    printf("END\n");
 
     return START;
 }
@@ -41,5 +55,13 @@ int main()
 {
     NODE *p = Add_AT_POS(NULL,12,0);
     printf("Start address is: %u \n",p);
+    NODE *q = Add_AT_POS(p,120,1);
+    printf("Start address is: %u \n",q);
+    NODE *r = Add_AT_POS(q,121,0);
+    printf("Start address is: %u \n",r);
+    NODE *s = Add_AT_POS(r,122,2);
+    printf("Start address is: %u \n",r);
+    NODE *t = Add_AT_POS(s,1212,7);
+    printf("Start address is: %u \n",r);
     return 0;
 }
--- SLL_Operations.c
+++ SLL_Operations.c
@@ -8,122 +8,33 @@ struct TAG
 
 typedef struct TAG NODE;
 
-NODE* ADD_AT_BEGIN(NODE *START, int X)
+NODE* ADD_AT_POS(NODE *START, int X, int POS)
 {
-    NODE *TEMP,*p;
+    NODE *TEMP, *p;
+    int i;
     TEMP = (NODE*)malloc(sizeof(NODE));
     TEMP -> DATA = X;
     TEMP -> LINK = NULL;
     if(START == NULL) START = TEMP;
-    else
+    else if(POS == 0)
     {
         TEMP -> LINK = START;
         START = TEMP;
     }
-
-    printf("\nDATA in Current Linked List is: \n");
-    p = START;
-    while (!(p -> LINK == NULL))
-    {
-        printf("%d \t", p->DATA);
-        p = p->LINK;
-    }    
-    printf("%d \n", p->DATA);  
-
-    return START;
-}
-
-NODE* ADD_AT_END(NODE* START, int X)
-{
-    NODE *TEMP, *p;
-    TEMP = (NODE*)malloc(sizeof(NODE));
-    TEMP -> DATA = X;
-    TEMP -> LINK = NULL;
-    if(START == NULL) START = TEMP;
     else
     {
         p = START;
-        while(p -> LINK != NULL) p = p -> LINK;
-        p -> LINK = TEMP;
-    }
-
-    p = START;
-    while (!(p -> LINK == NULL))
-    {
-        printf("%d\t", p -> DATA);
-        p = p -> LINK;
-    }
-    printf("%d\n", p -> DATA);
-
-    return START;
-}
-
-NODE* Add_AT_POS(NODE *START, int X, int POS)
-{
-    int i;
-    NODE *p, *TEMP;
-    TEMP = (NODE *)malloc(sizeof(NODE));
-    TEMP -> DATA = X;
-    TEMP -> LINK = NULL;
-
-    if(START == NULL) START = TEMP;
-    else
-    {
-        p = START;
-        for(i = 1; i <= POS; i++) p = p -> LINK;
+        for(i = 1; i < POS; i++)
+        {
+            p = p -> LINK;
+            if(p == NULL)
+            {
+                printf("Invalid Position\n");
+                return START;
+            }
+        }
         TEMP -> LINK = p -> LINK;
         p -> LINK = TEMP;
     }
-
-    p = START;
-    while (!(p -> LINK == NULL))
-    {
-        printf("%d\t", p -> DATA);
-        p = p -> LINK;
-    }
-    printf("%d\n", p -> DATA);
-
     return START;
-}
-
-int Count(NODE *START)
-{
-    int cnt = 1;
-    NODE *p = START;
-    if(START == NULL) return 0;
-    while(p -> LINK != NULL)
-    {
-        p = p -> LINK;
-        cnt++;
-    }
-    return cnt;
-}
-
-int main()
-{
-    int POS, X;
-    NODE *G;
-    printf("Enter DATA: ");
-    scanf("%d", &X);
-    printf("Enter Position: ");
-    scanf("%d", &POS);
-    if(POS == 0)
-    {
-        G = ADD_AT_BEGIN(NULL,X);
-        printf("Adding at begin: ", G);
-    }
-    else if(POS == Count(NULL))
-    {
-        G = ADD_AT_BEGIN(NULL,X);
-        G = ADD_AT_END(G, X);
-        printf("Adding at end: ", G);
-    }
-    else if(POS > Count(NULL)) printf("\n Cannot add");
-    else
-    {
-        G = Add_AT_POS(NULL,12,2);
-        printf("Adding at position: ", G);
-    }
-    return 0;
-
 }
