5dfbe29c3fe8a13966c83f73bd2597674db79659
darsigangothri06 <darsigangothri@gmail.com>
2022-02-16 17:56:43 +0530

    Add: CLL Add At End

---
+++ CLL_AddAtEnd.c
@@ -0,0 +1,56 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct TAG
+{
+    int DATA;
+    struct TAG *LINK;
+};
+
+typedef struct TAG NODE;
+
+NODE *START = NULL;
+
+void CREATE(int X)
+{
+    NODE *TEMP, *p;
+
+    TEMP = (NODE*)malloc(sizeof(NODE));
+    TEMP -> DATA = X;
+    TEMP -> LINK = START;
+
+    if(START == NULL) 
+    {
+        START = TEMP;
+        START -> LINK = START;
+    }
+    else
+    {
+        p = START;
+        while(p -> LINK != START)
+            p = p -> LINK;
+        p -> LINK = TEMP;
+    }
+}
+
+void DISPLAY()
+{
+    NODE *p = START;
+    printf("\nBEGIN -> ");
+    while(p -> LINK != START)
+    {
+        printf("%d ->", p -> DATA);
+        p = p -> LINK;
+    }
+    printf("%d -> END\n", p -> DATA);
+}
+
+void main()
+{
+    CREATE(10);
+    CREATE(20);
+    CREATE(30);
+    CREATE(40);
+    CREATE(50);
+    DISPLAY();
+}
